a40620abfe27bb243670c3fdbc25ab826ce1b312
Leandro Andreotti <landreotti91@gmail.com>
2024-10-06 17:38:46 -0300

    Add API, Data, posts e comment

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Blog4Devs.Data;
+using Blog4Devs.Models;
+
+namespace Blog4Devs.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Posts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Posts.ToListAsync());
+        }
+
+        // GET: Posts/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var posts = await _context.Posts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (posts == null)
+            {
+                return NotFound();
+            }
+
+            return View(posts);
+        }
+
+        // GET: Posts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Posts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,Status,CreatedBy,CreatedAt")] Posts posts)
+        {
+            if (ModelState.IsValid)
+            {
+                posts.Id = Guid.NewGuid();
+                _context.Add(posts);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(posts);
+        }
+
+        // GET: Posts/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var posts = await _context.Posts.FindAsync(id);
+            if (posts == null)
+            {
+                return NotFound();
+            }
+            return View(posts);
+        }
+
+        // POST: Posts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Title,Description,Status,CreatedBy,CreatedAt")] Posts posts)
+        {
+            if (id != posts.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(posts);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostsExists(posts.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(posts);
+        }
+
+        // GET: Posts/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var posts = await _context.Posts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (posts == null)
+            {
+                return NotFound();
+            }
+
+            return View(posts);
+        }
+
+        // POST: Posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var posts = await _context.Posts.FindAsync(id);
+            if (posts != null)
+            {
+                _context.Posts.Remove(posts);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PostsExists(Guid id)
+        {
+            return _context.Posts.Any(e => e.Id == id);
+        }
+    }
+}
